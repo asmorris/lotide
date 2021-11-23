7d40b70182278c1f0a01677da228908f4bc76ecc
Andrew Morrison <morro141@gmail.com>
2021-11-22 22:47:23 -0500

    update findKeyTest to use M&C

--- test/findKeyTest.js
+++ test/findKeyTest.js
@@ -1,45 +1,38 @@
-import assertEqual from "../assertEqual";
-import findKey from "../findKey";
+const assert = require("chai").assert;
+const findKey = require("../findKey");
 
-assertEqual(
-  findKey(
-    {
-      "Blue Hill": { stars: 1 },
-      Akaleri: { stars: 3 },
-      noma: { stars: 2 },
-      elBulli: { stars: 3 },
-      Ora: { stars: 2 },
-      Akelarre: { stars: 3 },
-    },
-    (x) => x.stars === 2
-  ),
-  "noma"
-);
+describe("#findKey", () => {
+  it("returns noma when key is noma", () => {
+    assert.deepEqual(
+      findKey(
+        {
+          "Blue Hill": { stars: 1 },
+          Akaleri: { stars: 3 },
+          noma: { stars: 2 },
+          elBulli: { stars: 3 },
+          Ora: { stars: 2 },
+          Akelarre: { stars: 3 },
+        },
+        (x) => x.stars === 2
+      ),
+      "noma"
+    );
+  });
 
-assertEqual(
-  findKey(
-    {
-      "Blue Hill": { stars: 1 },
-      Akaleri: { stars: 3 },
-      noma: { stars: 2 },
-      elBulli: { stars: 3 },
-      Ora: { stars: 2 },
-      Akelarre: { stars: 3 },
-    },
-    (x) => x.stars === 7
-  ),
-  undefined
-);
-
-assertEqual(
-  findKey(
-    {
-      Toronto: { housePrice: 1000000 },
-      Ottawa: { housePrice: 800000 },
-      Vancouver: { housePrice: 1400000 },
-      Moncton: { housePrice: 500000 },
-    },
-    (x) => x.housePrice === 800000
-  ),
-  "Ottawa"
-);
+  it("returns undefined when key does not exist", () => {
+    assert.deepEqual(
+      findKey(
+        {
+          "Blue Hill": { stars: 1 },
+          Akaleri: { stars: 3 },
+          noma: { stars: 2 },
+          elBulli: { stars: 3 },
+          Ora: { stars: 2 },
+          Akelarre: { stars: 3 },
+        },
+        (x) => x.stars === 7
+      ),
+      undefined
+    );
+  });
+});
