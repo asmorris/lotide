085e68dce6647cce43acd56b53029c1cada23fbd
Andrew Morrison <morro141@gmail.com>
2021-11-22 21:53:33 -0500

    update middleTest to use M&C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,28 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle(["hello", "world", "foo", 4]), ["world", "foo"]);
-assertArraysEqual(middle(["hello", "world", "foo", 4, 5]), ["foo"]);
+describe("#middle", () => {
+  it("returns an empty array for a single valued array", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns an empty array for a 2 value array", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns the middle item of an odd numbered array", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns the middle 2 values of an even numbered array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns the middle value of an array of strings", () => {
+    assert.deepEqual(middle(["foo", "bar", "baz"]), ["bar"]);
+  });
+
+  it("returns the middle value of a mixed values array", () => {
+    assert.deepEqual(middle(["foo", 2, "bar"]), [2]);
+  });
+});
